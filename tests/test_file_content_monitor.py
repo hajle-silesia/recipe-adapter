dd8232423b52ccc6a43e55baf5ae45bc68b70d9b
mtweeman <mtweeman@gmail.com>
2022-06-04 12:34:20 +0200

    Forward content (#5)
    * Moved test case independent methods to class methods
    
    * Added code tests coverage in CI pipeline
    
    * Added exploratory API endpoint
    
    * Added notification functionality

--- tests/test_file_content_monitor.py
+++ tests/test_file_content_monitor.py
@@ -1,81 +1,125 @@
-import unittest
-from time import sleep
 import os
+import unittest
 from pathlib import Path
+from time import sleep
+from unittest import mock
 
 from src.file_content_monitor import FileContentMonitor
 
+url = "http://subscriber/update"
 
+
+def mocked_requests_post(*args, **kwargs):
+    class MockResponse:
+        def __init__(self, json_data, status_code):
+            self.json_data = json_data
+            self.status_code = status_code
+
+        def json(self):
+            return self.json_data
+
+    if args[0] == url:
+        return MockResponse(None, 204)
+    else:
+        return MockResponse(None, 404)
+
+
+@mock.patch("src.file_content_monitor.requests.post", side_effect=mocked_requests_post)
 class TestFileContentMonitor(unittest.TestCase):
-    def setUp(self):
-        self.set_test_arguments()
-        self.set_tested_objects()
-        self.set_test_expected_results()
+    path = None
 
-    def tearDown(self):
-        self.remove_file()
+    @classmethod
+    def setUpClass(cls):
+        cls.set_test_arguments()
+        cls.set_tested_objects()
+        cls.set_test_expected_results()
 
-    def set_test_arguments(self):
-        self.path = Path(__file__).parent / "./test.txt"
-        self.content = ".123inside\nfile"
+    @classmethod
+    def set_test_arguments(cls):
+        cls.path = Path(__file__).parent / "./test.txt"
+        cls.content = ".123inside\nfile"
 
-    def set_tested_objects(self):
-        self.file_content_monitor = FileContentMonitor(self.path)
-
-    def set_test_expected_results(self):
+    @classmethod
+    def set_tested_objects(cls):
         pass
 
-    def test_Should_GetEmptyContent_When_FileIsNotAvailable(self):
+    @classmethod
+    def set_test_expected_results(cls):
+        pass
+
+    def setUp(self):
+        super().setUp()
+
+        self.file_content_monitor = FileContentMonitor(self.path, url)
+
+    def tearDown(self):
+        super().tearDown()
+
+        self.remove_file()
+
+    def test_Should_GetEmptyContent_When_FileIsNotAvailable(self, mock_get):
         wait_monitoring_interval_time_with_buffer()
 
         self.assertEqual("", self.file_content_monitor.content)
+        self.assertEqual(None, self.file_content_monitor.response)
 
-    def test_Should_GetEmptyContent_When_NotAvailableFileIsCreated(self):
+    def test_Should_GetEmptyContent_When_NotAvailableFileIsCreated(self, mock_get):
         wait_monitoring_interval_time_with_buffer()
         self.write_nothing_to_file_and_wait_monitoring_interval_time_with_buffer()
 
         self.assertEqual("", self.file_content_monitor.content)
+        self.assertEqual(None, self.file_content_monitor.response)
 
-    def test_Should_GetEmptyContent_When_FileIsEmpty(self):
+    def test_Should_GetEmptyContent_When_FileIsEmpty(self, mock_get):
         self.write_nothing_to_file_and_wait_monitoring_interval_time_with_buffer()
 
         self.assertEqual("", self.file_content_monitor.content)
+        self.assertEqual(None, self.file_content_monitor.response)
 
-    def test_Should_GetContent_When_EmptyFileContentIsUpdated(self):
+    def test_Should_GetContent_When_EmptyFileContentIsUpdated(self, mock_get):
         self.write_nothing_to_file_and_wait_monitoring_interval_time_with_buffer()
         self.append_content_to_file_and_wait_monitoring_interval_time_with_buffer()
 
         self.assertEqual(self.content, self.file_content_monitor.content)
+        self.assertEqual(204, self.file_content_monitor.response.status_code)
+        self.assertEqual(None, self.file_content_monitor.response.json())
 
-    def test_Should_GetContent_When_FileHasContent(self):
+    def test_Should_GetContent_When_FileHasContent(self, mock_get):
         self.write_content_to_file_and_wait_monitoring_interval_time_with_buffer()
 
         self.assertEqual(self.content, self.file_content_monitor.content)
+        self.assertEqual(204, self.file_content_monitor.response.status_code)
+        self.assertEqual(None, self.file_content_monitor.response.json())
 
-    def test_Should_GetEmptyContent_When_FileContentWasRemoved(self):
+    def test_Should_GetEmptyContent_When_FileContentWasRemoved(self, mock_get):
         self.write_content_to_file_and_wait_monitoring_interval_time_with_buffer()
         self.write_nothing_to_file_and_wait_monitoring_interval_time_with_buffer()
 
         self.assertEqual("", self.file_content_monitor.content)
+        self.assertEqual(None, self.file_content_monitor.response)
 
-    def test_Should_GetEmptyContent_When_FileWasRemoved(self):
+    def test_Should_GetEmptyContent_When_FileWasRemoved(self, mock_get):
         self.write_content_to_file_and_wait_monitoring_interval_time_with_buffer()
         self.remove_file()
         wait_monitoring_interval_time_with_buffer()
 
         self.assertEqual("", self.file_content_monitor.content)
+        self.assertEqual(None, self.file_content_monitor.response)
 
-    def test_Should_GetTheSameContent_When_FileWasSavedWithoutChangingContent(self):
+    def test_Should_GetTheSameContent_When_FileWasSavedWithoutChangingContent(self, mock_get):
         self.write_content_to_file_and_wait_monitoring_interval_time_with_buffer()
         self.append_nothing_to_file_and_wait_monitoring_interval_time_with_buffer()
 
         self.assertEqual(self.content, self.file_content_monitor.content)
+        self.assertEqual(None, self.file_content_monitor.response)
 
-    def test_Should_GetUpdatedContent_When_FileContentWasAdded(self):
+    def test_Should_GetUpdatedContent_When_FileContentWasAdded(self, mock_get):
         self.write_content_to_file_and_wait_monitoring_interval_time_with_buffer()
         self.append_content_to_file_and_wait_monitoring_interval_time_with_buffer()
 
         self.assertEqual(2 * self.content, self.file_content_monitor.content)
+        self.assertEqual(204, self.file_content_monitor.response.status_code)
+        self.assertEqual(None, self.file_content_monitor.response.json())
 
     def write_content_to_file_and_wait_monitoring_interval_time_with_buffer(self):
         with open(self.path, "w", encoding="utf-8") as file:
@@ -106,7 +150,3 @@ def wait_monitoring_interval_time_with_buffer():
     monitoring_interval_time = 5
     buffer = 1
     sleep(monitoring_interval_time + buffer)
-
-
-if __name__ == "__main__":
-    unittest.main()
