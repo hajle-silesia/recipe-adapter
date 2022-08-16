7e2d946cc68760ea6831d7ad8c50861e6a2e36ea
mtweeman <mtweeman@gmail.com>
2022-08-16 21:05:43 +0200

    Added default serialization for JSON

--- src/notifier.py
+++ src/notifier.py
@@ -35,4 +35,4 @@ class Notifier:
         self.responses.clear()
 
         for url in self.observers.values():
-            self.responses.append(requests.post(url, base64.b64encode(json.dumps(data).encode())))
+            self.responses.append(requests.post(url, base64.b64encode(json.dumps(data, default=str).encode())))
