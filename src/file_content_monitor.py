9471a34dede4c1ecd59af1bd69c5ba6d95933bcb
mtweeman <mtweeman@gmail.com>
2025-01-15 21:44:58 +0100

    feat: change file content monitoring to triggering (#11)

--- src/file_content_monitor.py
+++ src/file_content_monitor.py
@@ -1,105 +1,23 @@
-import hashlib
-import os
-import threading
-import time
-
-
-class FileContentMonitor(threading.Thread):
+class FileContentMonitor:
     _content_default = ""
 
     def __init__(self, producer):
-        threading.Thread.__init__(self, daemon=True)
+        self._producer = producer
 
-        self.__producer = producer
-
-        self.__path = ""
-        self.__monitoring_interval_time = 5
-        self.__last_modification_time = 0
-        self.__checksum = None
-        self.__content = self._content_default
-
-        self.start()
-
-    @property
-    def path(self):
-        return self.__path
-
-    @path.setter
-    def path(self, path):
-        self.__path = path
+        self._content = self._content_default
 
     @property
     def content(self):
-        return self.__content
+        return self._content
 
-    def run(self):
-        while True:
-            self.__monitor_file()
+    def update(self, new_raw_content):
+        self._update_data(new_raw_content)
+        self._notify()
 
-    def __monitor_file(self):
-        if os.path.exists(self.__path):
-            if self.__file_changed():
-                self.__update_data()
-                self.__notify()
-        else:
-            self.__clear_content()
+    def _update_data(self, new_raw_content):
+        self._content = new_raw_content
 
-        self.__wait_time_interval()
-
-    def __file_changed(self):
-        self.__get_last_modification_time()
-
-        if self.__modification_time_changed():
-            self.__calculate_file_checksum()
-
-            if self.__checksum_changed():
-                return True
-
-        return False
-
-    def __get_last_modification_time(self):
-        self.__new_last_modification_time = os.path.getmtime(self.__path)
-
-    def __modification_time_changed(self):
-        return True if self.__new_last_modification_time > self.__last_modification_time else False
-
-    def __calculate_file_checksum(self):
-        self.__read_file()
-        self.__get_checksum()
-
-    def __read_file(self):
-        with open(self.__path, encoding="utf-8") as file:
-            self.__new_content = file.read()
-
-    def __get_checksum(self):
-        self.__new_checksum = hashlib.sha256(self.__new_content.encode()).hexdigest()
-
-    def __checksum_changed(self):
-        return True if self.__new_checksum != self.__checksum else False
-
-    def __update_data(self):
-        self.__update_last_modification_time()
-        self.__update_checksum()
-        self.__update_content()
-
-    def __update_last_modification_time(self):
-        self.__last_modification_time = self.__new_last_modification_time
-
-    def __update_checksum(self):
-        self.__checksum = self.__new_checksum
-
-    def __update_content(self):
-        self.__content = self.__new_content
-
-    def __notify(self):
-        if self.content:
-            self.__producer.send(topic="file-content-monitor-topic",
-                                 value=self.content,
-                                 )
-
-    def __clear_content(self):
-        if self.content != self._content_default:
-            self.__content = self._content_default
-
-    def __wait_time_interval(self):
-        time.sleep(self.__monitoring_interval_time)
+    def _notify(self):
+        self._producer.send(topic="file-content-monitor-topic",
+                            value=self.content,
+                            )
